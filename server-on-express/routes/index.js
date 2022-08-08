ff9a5a841f56c61b1b329fd2de2aabc420c70377
Akatosh-kiber18 <cemper300@gmail.com>
2022-08-08 13:20:28 +0300

    add lists of task on express

--- server-on-express/routes/index.js
+++ server-on-express/routes/index.js
@@ -6,24 +6,13 @@ const inc = (init = 0) => () => ++init
 const genListId = inc();
 const genTaskId = inc();
 
-/*const tasks = [
-    {
-        id: genId(),
-        name: 'Get task'
-    },
-    {
-        id: genId(),
-        name: 'Create task'
-    },
-];*/
-
 const lists = [
     {
         id: genListId(),
         listName: 'Tomorrow tasks',
         tasks: [
             {
-                index: genTaskId(),
+                id: genTaskId(),
                 name: 'Buy some milk',
                 done: false
             }
@@ -33,7 +22,7 @@ const lists = [
 
 const createTask = data => {
     return {
-        index: genTaskId(),
+        id: genTaskId(),
         name: data.name,
         done: false,
     }
@@ -42,36 +31,68 @@ const createList = data => {
     return {
         id: genListId(),
         listName: data.listName,
-        tasks: data.tasks
+        tasks: data.tasks ? data.tasks: []
     }
 }
-router.get('/lists', (req, res) => res.json(lists));
+router.get('/lists', (req, res) => res.json(lists));//http localhost:3000/lists
 
-router.post('/lists', (req, res) => {
-    let list = createList(req.body);
-    lists.push(list);
-    res.json(list);
-});
-
-router.patch('/lists/:id', (req, res) => {
+router.get('/lists/:id', (req, res) => {//http localhost:3000/lists/1
     const listId = parseInt(req.params.id);
     const list = lists.find(l => l.id === listId);
-    const task = createTask(req.body);
+    res.json(list)
+});
+
+router.get('/tasks', (req, res) => {//http localhost:3000/tasks?listId=1
+    const listId = parseInt(req.query['listId']);
+    const list = lists.find(l => l.id === listId);
+    res.json(list.tasks)
+});
+
+router.get('/tasks/:id', (req, res) => {//http localhost:3000/tasks/1?listId=1
+    const listId = parseInt(req.query['listId']);
+    const list = lists.find(l => l.id === listId);
+
+    const taskId = parseInt(req.params.id);
+    const task = list.tasks.find(t => t.id === taskId);
+
+    res.json(task)
+});
+
+
+router.post('/lists', (req, res) => {//http localhost:3000/lists listName=ToDo
+    let list = createList(req.body);
+    lists.push(list);
+    res.json(lists);
+});
+
+router.post('/tasks', (req, res) => {//http localhost:3000/tasks?listId=1 name="task" done=false
+    const listId = parseInt(req.query['listId']);
+    const list = lists.find(l => l.id === listId);
+    if (list) {
+        const task = createTask(req.body);
+        list.tasks.push(task);
+    }
+    res.json(lists);
+});
+
+router.patch('/lists/:id', (req, res) => {//http PATCH localhost:3000/lists/1 listName="Done"
+    const listId = parseInt(req.params.id);
+    const list = lists.find(l => l.id === listId);
 
     if (list) {
-        list.tasks.push(task);
+        Object.assign(list, req.body);
         res.json(list);
     } else {
-        res.status(404).json({error: 'Task not found'});
+        res.status(404).json({error: 'List not found'});
     }
 });
 
-router.patch('/lists/:id/tasks/:index', (req, res) => {
-    const listId = parseInt(req.params.id);
+router.patch('/tasks/:id', (req, res) => {//http PATCH localhost:3000/tasks/1?listId=1 name="wth" done=true
+    const listId = parseInt(req.query['listId']);
     const list = lists.find(l => l.id === listId);
 
-    const taskId = parseInt(req.params.index);
-    const task = list.tasks.find(t => t.index === taskId);
+    const taskId = parseInt(req.params.id);
+    const task = list.tasks.find(t => t.id === taskId);
 
     if (task) {
         Object.assign(task, req.body);
@@ -81,35 +102,59 @@ router.patch('/lists/:id/tasks/:index', (req, res) => {
     }
 });
 
-router.delete('/lists/:id/tasks/:index', (req, res) => {
+router.delete('/lists/:id', (req, res) => {//http DELETE localhost:3000/lists/1
     const listId = parseInt(req.params.id);
     const list = lists.find(l => l.id === listId);
 
-    const taskId = parseInt(req.params.index);
-    const task = list.tasks.find(t => t.index === taskId);
-    if (task) {
-        list.tasks.splice(taskId - 1, 1)
-        res.json(list.tasks);
+    if (list) {
+        lists.splice(lists.indexOf(list), 1)
+        res.json(lists);
     } else {
-        res.status(404).json({error: 'Task not found'});
-    }
+            res.status(404).json({error: 'List not found'});
+        }
 })
-/*
-router.post('/tasks', (req, res) => {
-    let task = createTask(req.body);
-    tasks.push(task);
-    res.json(task);
-});
 
+router.delete('/tasks/:id', (req, res) => {//http DELETE localhost:3000/tasks/1?listId=1
+    const listId = parseInt(req.query['listId']);
+    const list = lists.find(l => l.id === listId);
 
+    const tasks = list.tasks;
 
-router.put('/tasks/:id', (req, res) => {
     const taskId = parseInt(req.params.id);
-    const task = tasks.find(t => t.id === taskId);
+    const task = tasks.find(t => parseInt(t.id) === taskId);
+
     if (task) {
-        tasks[taskId-1] = req.body;
+        tasks.splice(tasks.indexOf(task), 1);
         res.json(tasks);
     } else {
         res.status(404).json({error: 'Task not found'});
     }
-})*/
+})
+
+router.put('/lists/:id',(req, res) => {//http PUT localhost:3000/lists/1 name="HelloMOTOROLAS"//
+    const listId = parseInt(req.params.id);
+    const list = lists.find(l => parseInt(l.id) === listId);
+
+    if (list) {
+        lists[lists.indexOf(list)] = req.body;
+        res.json(lists);
+    } else {
+        res.status(404).json({error: 'List not found'});
+    }
+})
+
+router.put('/tasks/:id', (req, res) => {//http PUT localhost:3000/tasks/1?listId=1 name="HelloMOTOROLAS" done=true oil="oil"
+    const listId = parseInt(req.query['listId']);
+    const list = lists.find(l => parseInt(l.id) === listId);
+
+    const tasks = list.tasks;
+
+    const taskId = parseInt(req.params.id);
+    const task = tasks.find(t => parseInt(t.id) === taskId);
+    if (task) {
+        tasks[tasks.indexOf(task)] = req.body;
+        res.json(tasks);
+    } else {
+        res.status(404).json({error: 'Task not found'});
+    }
+})
